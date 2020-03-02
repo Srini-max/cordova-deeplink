d7cb8f829c8fc85ae35687a43f5684f8e2a54855
Srini-max <54628670+Srini-max@users.noreply.github.com>
2020-03-02 11:08:23 +0400

    Update Assets.java

--- src/android/Assets.java
+++ src/android/Assets.java
@@ -1,12 +1,12 @@
 /**
 	com.max.deeplink, ver. 6.1.6
-	https://github.com/Srini-max/deeplink-cordova-tst
+	https://github.com/Srini-max/cordova-deeplink
 	
 	Phonegap plugin for check or launch other application in android device (iOS support).
-	bug tracker: https://github.com/Srini-max/deeplink-cordova-tst/issues
+	bug tracker: https://github.com/Srini-max/cordova-deeplink/issues
 */
 
-package com.max.deeplink;
+package com.gems.deeplink;
 
 import android.content.Intent;
 import android.util.Log;
